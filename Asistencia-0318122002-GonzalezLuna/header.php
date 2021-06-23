cdd5fafd13b54b51d6f91783e9f9a04c83cbf032
BrendaGonzalezLuna <83687849+BrendaGonzalezLuna@users.noreply.github.com>
2021-06-22 22:40:25 -0500

    Add files via upload

---
+++ Asistencia-0318122002-GonzalezLuna/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>0318122002-BrendaGonzalez</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <style>
+        body {
+            padding-top: 100px;
+        }
+    </style>
+</head>
+<body>
+<main class="container-fluid">
